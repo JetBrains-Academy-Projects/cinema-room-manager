ae9db5bfa9e3f5555a76ad5b16026bf12d144b0a
Ujjal Khadka <khadkauzal@gmail.com>
2023-03-24 19:38:42 +0545

    completed-3/5

--- Cinema.java
+++ Cinema.java
@@ -1,32 +1,87 @@
 import java.util.Scanner;
 
 public class Cinema {
+    private final boolean[][] seats = new boolean[9][9];
+    private final int ticketPriceHigh = 10;
+    private final int ticketPriceLow = 8;
+    private final int m;
+    private final int n;
 
-    public static void main(String[] args) {
-        int ticketPriceHigh = 10;
-        int ticketPriceLow = 8;
-        Scanner scanner = new Scanner(System.in);
-        int totalIncome;
+    public Cinema(int m, int n){
+        this.m = m;
+        this.n = n;
+    }
 
-        System.out.println("Enter the number of rows:");
-        int rows = scanner.nextInt();
-
-        System.out.println("Enter the number of seats in each row:");
-        int seatsPerRow = scanner.nextInt();
-
-        if (rows * seatsPerRow <= 60) {
-            totalIncome = rows * seatsPerRow * ticketPriceHigh;
-        } else {
-            if (rows % 2 == 0) {
-                totalIncome = (rows / 2) * seatsPerRow * ticketPriceHigh;
-                totalIncome += (rows / 2) * seatsPerRow * ticketPriceLow;
-            } else {
-                int frontRows = (rows / 2);
-                totalIncome = frontRows * seatsPerRow * ticketPriceHigh;
-                totalIncome += (rows - frontRows) * seatsPerRow * ticketPriceLow;
+    public void initializeSeats() {
+        for(int i=0; i<this.m; i++){
+            for(int j=0; j<this.n; j++){
+                this.seats[i][j] = true; // true means available
             }
         }
+    }
 
-        System.out.println("Total income:\n$" + totalIncome);
+    public void printSeats() {
+        System.out.println("\nCinema:");
+        System.out.print(" ");
+        for(int i=0; i<this.n; i++){
+            System.out.print(" " + (i + 1));
+        }
+        System.out.println();
+
+
+        for(int i=0; i<this.m; i++){
+            System.out.print(i+1);
+            for(int j=0; j<this.n; j++){
+                char seat = this.seats[i][j] ? 'S' : 'B';
+                System.out.print(" " + seat);
+            }
+            System.out.println();
+        }
+    }
+
+    public int bookSeat(int i, int j){
+        // book seat
+        this.seats[i-1][j-1] = false;
+
+        // calculate the ticket price
+        int ticketPrice = 0;
+
+        if (this.m * this.n <= 60) {
+            ticketPrice = this.ticketPriceHigh;
+        } else {
+            int frontRows = (this.m / 2);
+            if(i <= frontRows) ticketPrice = this.ticketPriceHigh;
+            else ticketPrice = this.ticketPriceLow;
+        }
+        return ticketPrice;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m, n;
+        int seatM, seatN;
+
+        System.out.println("Enter the number of rows:");
+        m = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        n = scanner.nextInt();
+
+        Cinema cinema = new Cinema(m, n);
+
+        cinema.initializeSeats();
+        cinema.printSeats();
+
+        System.out.println();
+        System.out.println("Enter a row number:");
+        seatM = scanner.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+        seatN = scanner.nextInt();
+
+        int ticketPrice = cinema.bookSeat(seatM, seatN);
+
+        System.out.println("Ticket price: $" + ticketPrice);
+        cinema.printSeats();
     }
 }
